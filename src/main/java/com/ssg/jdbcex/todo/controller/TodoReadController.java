e457148ca9c6ab3d38294275b4f343e3ba0bde26
thatzcool <mycaptain0771@gmail.com>
2025-04-09 15:56:30 +0900

    [CookielogInOut]

--- src/main/java/com/ssg/jdbcex/todo/controller/TodoReadController.java
+++ src/main/java/com/ssg/jdbcex/todo/controller/TodoReadController.java
@@ -6,6 +6,7 @@ import com.ssg.jdbcex.todo.service.TodoService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,6 +29,25 @@ public class TodoReadController extends HttpServlet {
             //모델 담기
             req.setAttribute("dto", todoDTO);
 
+            //cookie 찾기
+            Cookie viewTodoCookie  = findCookie(req.getCookies(),"viewTodos");
+            String todoListStr = viewTodoCookie.getValue();
+            boolean exists = false;
+            if(todoListStr != null && todoListStr.indexOf(tno+"-") >= 0){
+                exists = true;
+            }
+
+            log.info("exists:" + exists);
+
+            if(!exists){
+                todoListStr += tno+"-";
+                viewTodoCookie.setValue(todoListStr);
+                viewTodoCookie.setMaxAge(60*60*24);
+                viewTodoCookie.setPath("/");
+                resp.addCookie(viewTodoCookie);
+                }
+
+
             req.getRequestDispatcher("/WEB-INF/todo/read.jsp").forward(req, resp);
 
         }catch(Exception e){
@@ -35,5 +55,26 @@ public class TodoReadController extends HttpServlet {
             throw new ServletException("read error");
         }
     }
+
+    private Cookie findCookie(Cookie[] cookies, String cookieName) {
+            Cookie cookie = null;
+
+            if(cookies != null && cookies.length > 0) {
+                for(Cookie c : cookies){
+                    if(c.getName().equals(cookieName)){
+                         cookie = c;
+                         break;
+                    }
+                }
+            }
+            if(cookie == null){
+                cookie = new Cookie(cookieName, "");
+                cookie.setPath("/");
+                cookie.setMaxAge(60*60*24);
+
+                            }
+        return cookie;
+    }
+
 }
 
